a8e12d6c24454d321ec588ec9ccad30244da9358
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-03-27 14:58:11 -0500

    Added logging to store

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../reducers";
+import { createLogger } from "redux-logger";
+import usersReducer from "../reducers/users.reducer";
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
-        counter: counterReducer
-    }
+        users: usersReducer
+    },
+    middleware: [createLogger()]
 });
