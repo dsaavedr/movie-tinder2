6d0615f35c00b14739633e3f8758e0e0417ec454
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-03-27 14:58:29 -0500

    Added user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,22 @@
+export class UserController {
+    login(username, password) {
+        return dispatch => {
+            dispatch(request({username}))
+
+            userService.login(username, password)
+                .then(
+                    user => {
+                        dispatch(success(user));
+                    },
+                    err => {
+                        displatch(failure(err.toString()));
+                        // dispatch(alertActions.error(error.toString()));
+                    }
+                )
+
+            function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
+            function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
+            function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
+        }
+    }
+}
