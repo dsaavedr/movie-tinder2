65b306e96f618127c4d2f38d30c3f95cf52aa7a0
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-03-30 15:32:26 -0500

    Added more granulated state through auth process

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -3,9 +3,15 @@ import { userConstants } from "../constants";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
+const initialState = {
+    loggedIn: user !== null,
+    user,
+    loggingIn: false
+};
+
 export const authSlice = createSlice({
     name: "authentication",
-    initialState: user ? { loggedIn: true, user } : {},
+    initialState,
     reducers: {
         [userConstants.LOGIN_REQUEST]: (state, action) => {
             // Redux Toolkit allows us to write "mutating" logic in reducers. It
@@ -13,17 +19,18 @@ export const authSlice = createSlice({
             // which detects changes to a "draft state" and produces a brand new
             // immutable state based off those changes
             state.loggingIn = true;
-            state.user = action.user;
         },
         [userConstants.LOGIN_SUCCESS]: (state, action) => {
+            state.loggingIn = false;
             state.loggedIn = true;
             state.user = action.user;
         },
         [userConstants.LOGIN_FAILURE]: (state, action) => {
-            state = {};
+            state.loggingIn = false;
         },
         [userConstants.LOGOUT]: (state, action) => {
-            state = {};
+            state.user = null;
+            state.loggedIn = false;
         }
     }
 });
