39fdc5238b09a9a8722f3011c52abd03ba9039ed
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-04-01 21:05:00 -0500

    Added stateful check of auth for automatic re-render

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import Footer from "./Footer";
 
 import Header from "./Header";
 
 export default function MainContent() {
-    const auth = localStorage.getItem("user");
+    const auth = useSelector(state => state.auth.user);
 
     if (!auth) return <Navigate to='/login' />;
 
